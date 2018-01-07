ee7e9e0df929f393f9063d05a4b3986ca8d4bf56
chaninkou <michaelkoulove@yahoo.com>
2018-01-06 23:05:42 -0800

    learning php

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
-<h1>Hello World </h1>
+<html>
+<head><title>Testing</title></head>
+<body>
 <?php
-echo "Hi there.";
+#array
+$people = array("Alex", "michael","emily");
+
+#loop
+foreach ($people as $person){
+	echo $person . ' ';
+}
+
+#print stuf
+echo "Hi there " . $people[2];
+
+$loggedIn = true;
+if($loggedIn == true){
+	echo "You are logged in";
+} else{
+	echo "log in please";
+}
 ?>
+</body>
+</html>
